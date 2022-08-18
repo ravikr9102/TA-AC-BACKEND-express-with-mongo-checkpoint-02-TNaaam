01d10be2eda162f8e5a7b48b903af76e656290ef
Ravi kumar <rkravikr9102@gmail.com>
2022-08-18 12:02:21 +0530

    refactor

--- block-BNaaft/eventmanagement/models/event.js
+++ block-BNaaft/eventmanagement/models/event.js
@@ -1,16 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var eventSchema = new Schema({
+var eventSchema = new Schema(
+  {
     title: { type: String, required: true },
     summary: String,
     host: String,
-    startDate: { type: Date},
-    endDate: { type: Date},
-    category: [ String ],
+    startDate: { type: Date },
+    endDate: { type: Date },
+    category: [String],
     location: String,
-    likes: { type: Number, default: 0},
-    remark: [{ type: Schema.Types.ObjectId, ref: 'Remark'}]
-},{timestamps: true});
+    likes: { type: Number, default: 0 },
+    remark: [{ type: Schema.Types.ObjectId, ref: 'Remark' }],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Event',eventSchema);
+module.exports = mongoose.model('Event', eventSchema);
